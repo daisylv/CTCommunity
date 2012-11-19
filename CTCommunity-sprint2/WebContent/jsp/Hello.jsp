94cfd75231135e1d2cda23aaf87b9d834bf8b2eb
MaoMao <403158647@qq.com>
2012-11-19 10:23:28 +0800

    可以将username存入数据库，并显示到JSP

---
+++ CTCommunity-sprint2/WebContent/jsp/Hello.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	Hello!Please enter your userId  sinaId and userName!
+	<ul>
+    <li>
+		<s:form action="AddUser" namespace="/com/action" method="post">
+		<!--<s:textfield label="userId" name="UserId"></s:textfield>
+			<s:textfield label="sinaId" name="SinaId"></s:textfield>-->
+			<s:textfield label="userId" name="User.userId"></s:textfield> 
+			<s:textfield label="sinaId" name="User.sinaId"></s:textfield>
+			<s:textfield label="username" name="User.username"></s:textfield>
+			<s:submit value="submit"></s:submit>
+		</s:form>
+	</li>
+</ul>
+</body>
+</html>
---
+++ CTCommunity-sprint2/src/org/hns/user/User.java
@@ -0,0 +1,69 @@
+package org.hns.user;
+
+public class User {
+	
+	private int userId;
+	private int sinaId;
+	private String username;
+	private String usergender;
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public void setSinaId(int sinaId) {
+		this.sinaId = sinaId;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setUsergender(String usergender) {
+		this.usergender = usergender;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public int getSinaId() {
+		return sinaId;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getUsergender() {
+		return usergender;
+	}
+	
+	/*public void setUserId(int userId){
+		this.userId = userId;
+	}
+	public int getUserId(){
+		return userId;
+	}
+	
+	public void setSinaId(int sinaId){
+		this.sinaId = sinaId;
+	}
+	public int getSinaId(){
+		return sinaId;
+	}
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+	public String getUsername(){
+		return username;
+	}
+	
+	public void setUsergender(String usergender){
+		this.usergender = usergender;
+	}
+	public String getUsergender(){
+		return usergender;
+	}*/
+}
